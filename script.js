dc45a3b55f49f82228557a01be9a73981252843d
Boldizsar Szabo <szboldisz@gmail.com>
2023-05-16 12:49:37 +0200

    Add functionality to Random Location button plus some error handling

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const country = document.getElementById("country");
 const city = document.getElementById("city");
 const randomBirthdayBtn = document.getElementById("random-birthday-btn");
 const randomLocationBtn = document.getElementById("random-location-btn");
-const randomInfo = document.getElementById("random-info");
+
+const ageYears = document.getElementById("age-years");
+const ageMonths = document.getElementById("age-months");
+const ageWeeks = document.getElementById("age-weeks");
+const ageDays = document.getElementById("age-days");
+
+const weatherError = document.getElementById("weather-error");
 const minTemp = document.getElementById("min-temp");
 const meanTemp = document.getElementById("mean-temp");
 const maxTemp = document.getElementById("max-temp");
@@ -14,10 +20,8 @@ const windSpeed = document.getElementById("wind-speed");
 const windGusts = document.getElementById("wind-gusts");
 const rain = document.getElementById("rain");
 const snow = document.getElementById("snow");
-const ageYears = document.getElementById("age-years");
-const ageMonths = document.getElementById("age-months");
-const ageWeeks = document.getElementById("age-weeks");
-const ageDays = document.getElementById("age-days");
+
+const randomInfo = document.getElementById("random-info");
 const randomBirths = [
   document.getElementById("random-birth-1"),
   document.getElementById("random-birth-2"),
@@ -34,6 +38,10 @@ const randomDeaths = [
 // Random birthday button
 randomBirthdayBtn.addEventListener("click", setRandomBirthday);
 
+// Random location button
+randomLocationBtn.addEventListener("click", setRandomLocation);
+
+// Form submit
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
@@ -43,24 +51,28 @@ form.addEventListener("submit", async (e) => {
   const birthDate = birthdayValue.slice(5).split("-");
 
   getAgeData(birthdayValue);
-
-  const [latitude, longitude] = await getCoordinates(
-    cityValue,
-    countryValue,
-    cityValue
-  );
-
-  getWeatherData(latitude, longitude, birthdayValue);
   getThisDayInfo(birthDate[0], birthDate[1]);
+
+  try {
+    const [latitude, longitude] = await getCoordinates(
+      cityValue,
+      countryValue,
+      cityValue
+    );
+    getWeatherData(latitude, longitude, birthdayValue);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Get city coordinates from Open-Meteo API
-const getCoordinates = async (city, targetCountry, targetCity) => {
-  let latitude, longitude;
-
+const getCoordinates = async (cityName, targetCountry, targetCity) => {
   try {
+    weatherError.textContent = "";
+    let latitude, longitude;
+
     const res = await fetch(
-      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`
+      `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=10&language=en&format=json`
     );
     const data = await res.json();
 
@@ -71,39 +83,49 @@ const getCoordinates = async (city, targetCountry, targetCity) => {
       ) {
         latitude = data.results[i].latitude;
         longitude = data.results[i].longitude;
+        country.value = data.results[i].country;
+        city.value = data.results[i].name;
         break;
       }
     }
     return [latitude, longitude];
   } catch (err) {
-    alert(err);
+    weatherError.textContent = "City not found, please try another one.";
   }
 };
 
 // Get weather data from Open-Meteo API
 async function getWeatherData(latitude, longitude, date) {
   try {
-    const res = await fetch(
-      `https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${date}&end_date=${date}&daily=weathercode,temperature_2m_max,temperature_2m_min,temperature_2m_mean,sunrise,sunset,rain_sum,snowfall_sum,windspeed_10m_max,windgusts_10m_max&timezone=auto`
-    );
-    const data = await res.json();
+    const year = date.slice(0, 4);
+    if (latitude === undefined || longitude === undefined) {
+      weatherError.textContent = "City not found, please try another one.";
+    } else if (year >= 1940) {
+      weatherError.textContent = "";
+      const res = await fetch(
+        `https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${date}&end_date=${date}&daily=weathercode,temperature_2m_max,temperature_2m_min,temperature_2m_mean,sunrise,sunset,rain_sum,snowfall_sum,windspeed_10m_max,windgusts_10m_max&timezone=auto`
+      );
+      const data = await res.json();
 
-    minTemp.textContent = data.daily.temperature_2m_min[0];
-    meanTemp.textContent = data.daily.temperature_2m_mean[0];
-    maxTemp.textContent = data.daily.temperature_2m_max[0];
-    if (date.slice(0, 4) >= 1970) {
-      sunrise.textContent = data.daily.sunrise[0].slice(-5);
-      sunset.textContent = data.daily.sunset[0].slice(-5);
+      minTemp.textContent = data.daily.temperature_2m_min[0];
+      meanTemp.textContent = data.daily.temperature_2m_mean[0];
+      maxTemp.textContent = data.daily.temperature_2m_max[0];
+      if (year >= 1970) {
+        sunrise.textContent = data.daily.sunrise[0].slice(-5);
+        sunset.textContent = data.daily.sunset[0].slice(-5);
+      } else {
+        sunrise.textContent = "No data";
+        sunset.textContent = "No data";
+      }
+      windSpeed.textContent = data.daily.windspeed_10m_max[0];
+      windGusts.textContent = data.daily.windgusts_10m_max[0];
+      rain.textContent = data.daily.rain_sum[0];
+      snow.textContent = data.daily.snowfall_sum[0];
     } else {
-      sunrise.textContent = "No data";
-      sunset.textContent = "No data";
+      weatherError.textContent = "Weather data is only available from 1940.";
     }
-    windSpeed.textContent = data.daily.windspeed_10m_max[0];
-    windGusts.textContent = data.daily.windgusts_10m_max[0];
-    rain.textContent = data.daily.rain_sum[0];
-    snow.textContent = data.daily.snowfall_sum[0];
   } catch (err) {
-    alert(err);
+    alert("There was an error with the weather data. Please try again.");
   }
 }
 
@@ -128,7 +150,7 @@ async function getThisDayInfo(month, day) {
       randomDeaths[i].textContent = `(${randomDeath.year}) ${randomDeath.text}`;
     }
   } catch (err) {
-    alert(err);
+    alert('There was an error with the "This day" data. Please try again.');
   }
 }
 
@@ -160,7 +182,27 @@ function setRandomBirthday() {
     if (randomDay < 10) randomDay = `0${randomDay}`;
 
     birthday.value = `${randomYear}-${randomMonth}-${randomDay}`;
+  }
+}
 
-    console.log(`${randomYear}-${randomMonth}-${randomDay}`);
+// Set random location
+async function setRandomLocation() {
+  try {
+    const res = await fetch("https://countriesnow.space/api/v0.1/countries");
+    const data = await res.json();
+    if (randomLocationBtn.checked) {
+      const randomCountry =
+        data.data[Math.floor(Math.random() * data.data.length)];
+      const randomCountryName = randomCountry.country;
+      const randomCity =
+        randomCountry.cities[
+          Math.floor(Math.random() * randomCountry.cities.length)
+        ];
+
+      country.value = randomCountryName;
+      city.value = randomCity;
+    }
+  } catch (err) {
+    alert("There was an error setting random location. Please try again.");
   }
 }
